3d8f1d41fd41008fc1eec195dc4f8c5778177b9b
Robert Hanna <rdhanna@gmail.com>
2023-09-17 11:34:27 -0400

    udpated search functionality to use regex

--- src/database/search.rs
+++ src/database/search.rs
@@ -1,7 +1,8 @@
 use crate::database::employee::Employee;
 use clearscreen::clear;
 use futures::TryStreamExt;
-use mongodb::{bson::doc, Client, options::FindOptions};
+use mongodb::{bson::doc, options::FindOptions, Client};
+use regex::Regex;
 use std::io;
 
 pub async fn search_employees(client: &Client) -> Result<(), mongodb::error::Error> {
@@ -10,30 +11,37 @@ pub async fn search_employees(client: &Client) -> Result<(), mongodb::error::Err
     let db = &client.database("Employee");
     let coll = db.collection::<Employee>("employees");
 
-    let mut employee = Employee {
-        name: String::new(),
-        title: String::new(),
-        salary: None,
-        phone: None,
-    };
+    let mut search_pattern = String::new();
 
     println!("Please enter employee name to search:");
     io::stdin()
-        .read_line(&mut employee.name)
+        .read_line(&mut search_pattern)
         .expect("You didn't enter an employee name");
-    employee.name = employee.name.trim().to_string();
+
+    search_pattern = search_pattern.trim().to_string();
+    let regex_pattern = Regex::new(&format!("{}", search_pattern)).unwrap();
+
+    let query = doc! {"name" : {
+    "$regex": regex_pattern.to_string(),    // take whatever they enter and search for those characters
+    "$options": "i"}}; // case insenstive
 
     let find_options = FindOptions::builder().sort(doc! {"name": 1}).build();
+
+    // return result sorted by name asc
     let employees: Vec<Employee> = coll
-        .find(doc! {"name" : &employee.name}, find_options)
+        .find(query, find_options)
         .await
         .expect("Unable to get cursor")
         .try_collect()
         .await
         .expect("Unable to get items in cursor");
 
-    for e in employees.iter() {
-        println!("Employee: {}, Title: {}", e.name, e.title);
+    if employees.len() > 0 {
+        for e in employees.iter() {
+            println!("Employee: {}, Title: {}", e.name, e.title);
+        }
+    } else {
+        println!("No records found");
     }
 
     Ok(())
